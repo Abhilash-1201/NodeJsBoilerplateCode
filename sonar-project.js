ef994808b3684bbd40a2ed75360a20f960c3efb5
Abhilash-1201 <83076098+Abhilash-1201@users.noreply.github.com>
2022-08-24 16:34:45 +0530

    Delete sonar-project.js

--- sonar-project.js
+++
@@ -1,8 +0,0 @@
-const sonarqubeScanner = require('sonar-scanner');
-     sonarqubeScanner({
-       serverUrl: 'http://54.216.155.5:9000',
-       options : {
-       'sonar.sources': '.',
-       'sonar.inclusions' : 'src/**' // Entry point of your code
-       }
-     }, () => {});
